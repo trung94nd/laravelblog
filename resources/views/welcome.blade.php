e2ba61dbdf72094f65f97eefbbc58b814b00dc95
trung94nd <trung94nd@gmail.com>
2017-02-20 15:51:05 +0700

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,12 @@
-Xin chào các bạn
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <h1> chao ban</h1>
+</body>
+</html>
